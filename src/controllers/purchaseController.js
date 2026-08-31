a5a1a432419011b95c09f022087f372c7946a785
Hyan07 <130313209+Hyan07@users.noreply.github.com>
2026-08-30 22:33:08 -0300

    feat: expose purchase confirmation endpoint

--- src/controllers/purchaseController.js
+++ src/controllers/purchaseController.js
@@ -1,5 +1,6 @@
 'use strict';
 const purchaseService = require('../services/purchaseService');
+const purchaseConfirmationService = require('../services/purchaseConfirmationService');
 async function list(req,res,next){try{res.json(await purchaseService.listPurchases(req.query));}catch(error){next(error);}}
 async function lookupSkus(req,res,next){try{res.json({data:await purchaseService.lookupSkus(req.query)});}catch(error){next(error);}}
 async function getById(req,res,next){try{res.json({data:await purchaseService.getPurchase(req.params.id)});}catch(error){next(error);}}
@@ -9,6 +10,7 @@ async function addItem(req,res,next){try{res.status(201).json({id:await purchase
 async function updateItem(req,res,next){try{await purchaseService.updatePurchaseItem(req.params.id,req.params.itemId,req.body,req.user);res.status(204).end();}catch(error){next(error);}}
 async function cancelItem(req,res,next){try{await purchaseService.cancelPurchaseItem(req.params.id,req.params.itemId,req.user);res.status(204).end();}catch(error){next(error);}}
 async function order(req,res,next){try{await purchaseService.orderPurchase(req.params.id,req.user);res.status(204).end();}catch(error){next(error);}}
+async function confirm(req,res,next){try{res.status(201).json({data:await purchaseConfirmationService.confirmPurchase(req.params.id,req.body,req.user)});}catch(error){next(error);}}
 async function cancel(req,res,next){try{await purchaseService.cancelPurchase(req.params.id,req.body,req.user);res.status(204).end();}catch(error){next(error);}}
 async function receive(req,res,next){try{res.status(201).json({data:await purchaseService.receivePurchase(req.params.id,req.body,req.user)});}catch(error){next(error);}}
-module.exports={addItem,cancel,cancelItem,create,getById,list,lookupSkus,order,receive,update,updateItem};
+module.exports={addItem,cancel,cancelItem,confirm,create,getById,list,lookupSkus,order,receive,update,updateItem};
