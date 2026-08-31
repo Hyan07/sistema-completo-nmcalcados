dbd4a68f05ad89ffd40762ba9b329d249932163f
Hyan07 <130313209+Hyan07@users.noreply.github.com>
2026-08-30 22:31:55 -0300

    feat: add quick grade route

--- src/routes/gradeRoutes.js
+++ src/routes/gradeRoutes.js
@@ -6,6 +6,7 @@ const { authorize } = require('../middlewares/authorize');
 
 const router = express.Router({ mergeParams: true });
 router.get('/', authorize('products.read'), controller.get);
+router.post('/quick', authorize('products.manage'), controller.createQuickGrade);
 router.patch('/images/:imageId/variant', authorize('products.manage'), controller.assignImageVariant);
 router.post('/variants', authorize('products.manage'), controller.createVariant);
 router.patch('/variants/:variantId', authorize('products.manage'), controller.updateVariant);
